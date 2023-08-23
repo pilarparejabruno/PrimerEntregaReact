5543f76868e5d8f03c04f351953a131182a2c2fb
Pilar Pareja Bruno <parejabrunopilar@gmail.com>
2023-08-23 19:59:49 -0300

    agregue el componente Video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import CartWidget from "./components/CartWidget/CartWidget";
+import Video from "./components/Video/Video";
 
 function App() {
   return (
     <header>
       <Navbar />;
       <CartWidget />
+      <Video />
     </header>
   );
 }
---
+++ src/components/Video/Video.js
@@ -0,0 +1,7 @@
+const Video = () => {
+  <video class="video mx-auto d-block" autoplay loop muted>
+    <source src="./videobackground.mp4" type="video/mp4" />
+  </video>;
+};
+
+export default Video;
