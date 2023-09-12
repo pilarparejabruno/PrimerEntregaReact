80aba2e5e39ff37d0c318aae5c213d3ded5984b3
Pilar Pareja Bruno <parejabrunopilar@gmail.com>
2023-09-12 13:40:59 -0300

    agregando categorias

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,27 @@
 import { useEffect, useState } from "react";
 import { getProducts } from "../../asyncMock";
 import ItemList from "../ItemList/ItemList";
+import { Link } from "react-router-dom";
 import "./ItemListContainer.css";
+
+const RoutesCategories = [
+  {
+    path: "/category/Cuidado facial",
+    linkname: "Cuidado facial",
+  },
+  {
+    path: "/category/Cuidado capilar",
+    linkname: "Cuidado capilar",
+  },
+  {
+    path: "/category/Cuidado labial",
+    linkname: "Cuidado labial",
+  },
+  {
+    path: "/category/Accesorios",
+    linkname: "Accesorios",
+  },
+];
 
 const ItemListContainer = ({ greeting }) => {
   const [products, setProducts] = useState([]);
@@ -31,10 +51,19 @@ const ItemListContainer = ({ greeting }) => {
     );
 
   return (
-    <div>
-      <h1 className="text-center  pb-5 mb-5">PRODUCTOS</h1>
+    <>
+      <h1 className="text-center mt-5 mb-2">PRODUCTOS</h1>
+      <div className="category-container text-center">
+        <div className="btn-group" role="group" aria-label="Basic example">
+          {RoutesCategories.map(({ path, linkname }) => (
+            <Link to={path} className="button btn btn-outline-dark">
+              {linkname}
+            </Link>
+          ))}
+        </div>
+      </div>
       <ItemList products={products} />
-    </div>
+    </>
   );
 };
 
